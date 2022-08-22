74f7ab2c65d83900ed6e6c4ad0961379b38ef6c7
m7real <masayeedreal@gmail.com>
2022-08-22 14:17:12 +0600

    li added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 const buttons = document.getElementsByClassName("btn-select");
+
 for (const button of buttons) {
   button.addEventListener("click", function () {
     const playerName = button.previousElementSibling.innerText;
+
+    // disable button when clicked
     button.setAttribute("disabled", true);
     button.style.backgroundColor = "gray";
+
+    // create li
+    const li = document.createElement("li");
+    li.innerText = playerName;
+    li.classList.add("ps-3");
+    // add li
+    const selectedContainer = document.getElementById("selected-container");
+    selectedContainer.appendChild(li);
   });
 }
